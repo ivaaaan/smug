c0664201d94bfcccbfd0134ca6a3f4000a6891cb
Ivan Klymenchenko <developer@iillexial.me>
2020-12-30 12:00:39 +0200

    Print version in the usage command

--- options.go
+++ options.go
@@ -6,7 +6,7 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
-const usage = `Smug - tmux session manager.
+const usage = `Smug - tmux session manager. Version v0.1.3
 
 Usage:
 	smug <command> <project> [-w <window>]... [--attach] [--debug]
